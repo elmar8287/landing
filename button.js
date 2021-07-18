bee380768eb619731764ba8c28079a649e4e67b3
elmar8287 <L-mar@inbox.ru>
2021-07-18 15:29:21 +0400

    Fix linter issues

--- button.js
+++ button.js
@@ -1,5 +1,1 @@
-export 
 
-// let xxx = 'Hello all';
-
-let aaa = () => alert('This is from export module');
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 import {aaa} from './button.js';
-
-
-
-let elmar = a => 'Hi, this is first article' + a;
-
-document.getElementById('content').innerHTML = elmar(' WELCOME');
-
